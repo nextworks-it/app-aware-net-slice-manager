b37b7a613e0b180bbccbcc6b0300be9348024695
Mike-no <deangelismichael98@gmail.com>
2022-03-25 23:07:45 +0100

    added logging

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import logging
 from flask import Flask
 from apis import api
+
+# configure root logger
+logging.basicConfig(
+    level=logging.INFO,
+    format='[%(asctime)s] - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 app = Flask(__name__)
 api.init_app(app)
