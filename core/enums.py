9c1e70e8ba7a00fb7b438f4ee171dc99ba92e72c
Mike-no <deangelismichael98@gmail.com>
2022-04-27 16:15:28 +0200

    enum to handle ns status notification

--- core/enums.py
+++ core/enums.py
@@ -18,3 +18,19 @@ class IsolationLevel(enum.Enum):
     NoIsolation = 1
     Logical = 2
     Physical = 3
+
+
+class NsiNotificationType(enum.Enum):
+    STATUS_CHANGED = 1
+    ERROR = 2
+
+
+class NsiStatus(enum.Enum):
+    CREATED = 1
+    INSTANTIATING = 2
+    INSTANTIATED = 3
+    CONFIGURING = 4
+    TERMINATING = 5
+    TERMINATED = 6
+    FAILED = 7
+    OTHER = 8
